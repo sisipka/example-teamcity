870c3dda270770f2e042dca4d3b86a7193d54a0c
Andrew Shestikhin <shestihinandrey@gmail.com>
2022-09-11 23:38:45 +0300

    update

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -31,7 +31,7 @@ public class WelcomerTest {
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
 	@Test
-	public void welcomerSaysHunter(){
-		assertThat(welcomer.saySome(), containsString("hunter"));
+	public void welcomerSaysHunter2(){
+		assertThat(welcomer.Hunter2(), containsString("hunter"));
 	}
 }
